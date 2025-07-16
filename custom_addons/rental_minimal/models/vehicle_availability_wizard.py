f4773755ea2eed9b848921363639e4494e2bd57c
hugozeballos <Hzeballosmeneses@gmail.com>
2025-07-16 12:45:02 -0400

    buscar disponibilidad entre

--- custom_addons/rental_minimal/models/vehicle_availability_wizard.py
+++ custom_addons/rental_minimal/models/vehicle_availability_wizard.py
@@ -1,31 +1,35 @@
 from odoo import models, fields, api
 from datetime import datetime
+from odoo.exceptions import ValidationError
+
 #Modelo para chekear disponibilidad de vehiculos
 class VehicleAvailabilityWizard(models.TransientModel):
     _name = 'vehicle.availability.wizard'
     _description = 'Buscar Disponibilidad de Vehículos'
 
-    date = fields.Date(string='Fecha de búsqueda', required=True)
+    start_date = fields.Date(string='Fecha inicio', required=True)
+    end_date = fields.Date(string='Fecha término', required=True)
 
     def action_check_availability(self):
         self.ensure_one()
-        date_only = self.date  # ya es date, pero lo aclaramos
-        date_str = date_only.strftime('%Y-%m-%d')
+        start_date = self.start_date
+        end_date = self.end_date
+
+        if end_date < start_date:
+            raise ValidationError("La fecha de término debe ser igual o posterior a la fecha de inicio.")
 
         vehicles = self.env['fleet.vehicle'].search([])
 
         available_vehicles = vehicles.filtered(lambda v: all(
-            booking.state == 'cancelled' or
-            booking.state == 'returned' or
-            not (
-                booking.date_start.date() <= date_only <= booking.date_end.date()
-            )
+            booking.state in ['cancelled', 'returned'] or
+            booking.date_end.date() < start_date or
+            booking.date_start.date() > end_date
             for booking in v.rental_booking_ids
         ) and v.is_available_for_rent)
 
         return {
             'type': 'ir.actions.act_window',
-            'name': f'Autos disponibles para {date_str}',
+            'name': f'Autos disponibles del {start_date} al {end_date}',
             'res_model': 'fleet.vehicle',
             'view_mode': 'tree,form',
             'domain': [('id', 'in', available_vehicles.ids)],
