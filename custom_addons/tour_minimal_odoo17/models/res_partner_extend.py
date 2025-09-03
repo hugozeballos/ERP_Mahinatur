4e8e9c1befbf0e266e1902667e981ba4c34928ae
hugozeballos <Hzeballosmeneses@gmail.com>
2025-09-03 12:03:39 -0400

    requerid=false en res_partned_extend

--- custom_addons/tour_minimal_odoo17/models/res_partner_extend.py
+++ custom_addons/tour_minimal_odoo17/models/res_partner_extend.py
@@ -2,4 +2,4 @@ from odoo import models, fields
 
 class ResPartner(models.Model):
     _inherit = 'res.partner'
-    is_hotel = fields.Boolean('Es hotel')
+    is_hotel = fields.Boolean('Es hotel', required=False)
